5e07eff30a2d9f1cba2d1ae310e58c96bb0bdfca
Kimi SARAZU <kimisaraz@gmail.com>
2014-02-18 03:42:57 +0900

    Set sass output style to :compact

--- config.rb
+++ config.rb
@@ -36,9 +36,9 @@ page "/feed.xml", layout: false
 ###
 
 # Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+compass_config do |config|
+  config.output_style = :compact
+end
 
 ###
 # Page options, layouts, aliases and proxies
